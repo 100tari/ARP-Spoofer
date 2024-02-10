bd7dae1292d4160825c08572b9f43d9bd0df1c6c
mohammad mahdi sattari <mamadsadtari83@gmail.com>
2024-02-10 14:28:59 -0500

    add sniff_arp_pkt function to do what we want
    + adding if statement in extract functions; if the packet is exactly
      what we want extract the data
    
    + changing return type of extract functions to the packet they are
      extracting
    
    + changing + to [+] in printing data

--- src/main.c
+++ src/main.c
@@ -4,16 +4,15 @@
 int main()
 {
     unsigned char buff[ETH_FRAME_LEN * sizeof(char)] = {0};
+    size_t buf_size;
     int sock_raw;
 
     sock_raw = init_capturing("wlan0");
 
     while(1)
     {
-        capture(sock_raw, buff, ETH_FRAME_LEN);
-        extract_arp_header(buff);
-        extract_arp_payload(buff);
-        printf("-----------------------------------------\n");
+        buf_size = capture(sock_raw, buff, ETH_FRAME_LEN);
+        sniff_arp_pkt(buff, buf_size);
     }
     
 }
