1dc64d106338c2a93fc5c9b11034521e1aec19cf
mohammad mahdi sattari <mamadsadtari83@gmail.com>
2024-02-10 16:38:17 -0500

    adding initilizer to main

--- src/main.c
+++ src/main.c
@@ -16,19 +16,18 @@ int main(int args, char* argv[])
 
     IP frst_ip;
     IP scnd_ip;
-    MAC my_mac, trgrt_mac;
+    MAC my_mac;
+    MAC trgt_mac;
     
     get_my_mac(argv[1], my_mac);
-
     str_to_ip(argv[2], frst_ip);
-
     str_to_ip(argv[3], scnd_ip);
 
-    int sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP));
+    int sock = init_spoofing(argv[1]);
 
     broadcast_spoofed_ip(my_mac, frst_ip, scnd_ip,get_interface_sending(argv[1]), sock);
-    get_target_mac(sock, scnd_ip, trgrt_mac);
-    send_spoofed_ip(my_mac, frst_ip, trgrt_mac, scnd_ip, get_interface_sending(argv[1]), sock);
+    get_target_mac(sock, scnd_ip, trgt_mac);
+    send_spoofed_ip(my_mac, frst_ip, trgt_mac, scnd_ip, get_interface_sending(argv[1]), sock);
 
     exit(EXIT_SUCCESS);
 }
